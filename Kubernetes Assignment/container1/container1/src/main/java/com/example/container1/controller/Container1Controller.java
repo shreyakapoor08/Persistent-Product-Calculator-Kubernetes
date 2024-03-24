706af3a55806fd40df7701933abb5e231070e814
shreya kapoor <31164665+shreyakapoor08@users.noreply.github.com>
2024-03-24 00:07:13 -0300

    uploads

--- Kubernetes Assignment/container1/container1/src/main/java/com/example/container1/controller/Container1Controller.java
+++ Kubernetes Assignment/container1/container1/src/main/java/com/example/container1/controller/Container1Controller.java
@@ -4,26 +4,22 @@ package com.example.container1.controller;
 import com.example.container1.entity.FileStorageRequest;
 import com.example.container1.entity.Container1Request;
 import com.example.container1.exception.InvalidInputException;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/container1")
 public class Container1Controller {
 
-//    private static final String CONTAINER2_URL = "http://container2-service:8080/calculatesum";
     private static final String CONTAINER2_URL = "http://localhost:8080/calculatesum";
-private static final String STORAGE_PATH = "E:\\DALHOUSIE COURSEWORK\\Cloud Computing\\Kubernetes Assignment\\container1\\container1\\";
-//    private static final String STORAGE_PATH = "/home/volume/";
+    private static final String STORAGE_PATH = "/Users/lib-user/Downloads/volume/";
 
     private final RestTemplate restTemplate;
 
@@ -34,46 +30,39 @@ private static final String STORAGE_PATH = "E:\\DALHOUSIE COURSEWORK\\Cloud Comp
     @PostMapping("/store-file")
     public ResponseEntity<Object> storeFile(@RequestBody FileStorageRequest request) {
         try {
-            validateFileStorageRequest(request);
+            validateInput(request.getFile());
 
-            // Store the file in the persistent storage
             String filePath = STORAGE_PATH + request.getFile();
             System.out.println("File path is: " + filePath);
             Files.write(Paths.get(filePath), request.getData().getBytes());
 
-            return ResponseEntity.ok(createFileStorageResponse(request.getFile(), "Success."));
+            return ResponseEntity.ok(createResponse(request.getFile(), "Success."));
         } catch (InvalidInputException | IOException e) {
-            return ResponseEntity.badRequest().body(createFileStorageResponse(request.getFile(), "Error while storing the file to the storage."));
+            return ResponseEntity.badRequest().body(createResponse(request.getFile(), "Error while storing the file to the storage."));
         }
     }
 
     @PostMapping("/calculate")
-    public ResponseEntity<Object> calculate(@RequestBody Container1Request request) {
+    public ResponseEntity<Map<String,Object>> calculate(@RequestBody Container1Request request) {
         try {
-            validateInput(request);
-
-            ResponseEntity<Integer> responseEntity = sendRequestToContainer2(request.getProduct(), request.getFile()); //filename
-
-            return ResponseEntity.status(responseEntity.getStatusCode()).body(createResult(request.getFile(), "", responseEntity.getBody()));
+            validateInput(request.getFile());
+            return sendRequestToContainer2(request.getFile(), request.getProduct());
         } catch (InvalidInputException e) {
-            return ResponseEntity.badRequest().body(createResult(request.getFile(), e.getMessage(), null));
+            Map<String,Object> response = new HashMap<>();
+            response.put("file", request.getFile());
+            response.put("message", e.getMessage());
+            return ResponseEntity.badRequest().body(response);
         }
     }
 
-    private void validateFileStorageRequest(FileStorageRequest request) throws InvalidInputException {
-        if (request.getFile() == null) {
+    private void validateInput(String fileName) throws InvalidInputException {
+        if (fileName == null) {
             throw new InvalidInputException("Invalid JSON input.");
         }
     }
 
-    private void validateInput(Container1Request request) throws InvalidInputException {
-        if (request.getFile() == null) {
-            throw new InvalidInputException("Invalid JSON input.");
-        }
-    }
+    private ResponseEntity< Map<String,Object> > sendRequestToContainer2(String file, String product) {
 
-    private ResponseEntity<Integer> sendRequestToContainer2(String product, String file) {
-        try {
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
 
@@ -81,28 +70,27 @@ private static final String STORAGE_PATH = "E:\\DALHOUSIE COURSEWORK\\Cloud Comp
             requestBody.put("file", file);
             requestBody.put("product", product);
 
-            HttpEntity<Map<String, String>> entity = new HttpEntity<>(requestBody, headers);
+            HttpEntity<Map<String, String>> requestEntity = new HttpEntity<>(requestBody, headers);
+        ParameterizedTypeReference<Map<String, Object>> responseType =
+                new ParameterizedTypeReference<Map<String, Object>>() {};
 
-            return restTemplate.postForEntity(CONTAINER2_URL, entity, Integer.class);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(-2);
+        // Make the POST request
+        try{
+            ResponseEntity<Map<String, Object>> responseEntity = restTemplate.exchange(
+                    CONTAINER2_URL,
+                    HttpMethod.POST,
+                    requestEntity,
+                    responseType);
+            return responseEntity;
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+            return  null;
         }
+
     }
 
-
-    private Map<String, Object> createResult(String fileName, String errorMessage, Integer sum) {
-        Map<String, Object> result = new HashMap<>();
-        result.put("file", fileName);
-        if (sum == null) {
-            result.put("error", errorMessage);
-        } else {
-            result.put("sum", sum);
-        }
-        return result;
-    }
-
-    private Map<String, Object> createFileStorageResponse(String fileName, String message) {
-        Map<String, Object> result = new HashMap<>();
+    private Map<String, String> createResponse(String fileName, String message) {
+        Map<String, String> result = new HashMap<>();
         result.put("file", fileName);
         result.put("message", message);
         return result;
--- Kubernetes Assignment/container2/container2/src/main/java/com/example/container2/controller/Container2Controller.java
+++ Kubernetes Assignment/container2/container2/src/main/java/com/example/container2/controller/Container2Controller.java
@@ -13,6 +13,7 @@ import java.io.StringReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,17 +21,19 @@ import java.util.Map;
 public class Container2Controller {
 
 //    private static final String STORAGE_PATH = "/home/volume/";
-private static final String STORAGE_PATH = "E:\\DALHOUSIE COURSEWORK\\Cloud Computing\\Kubernetes Assignment\\container1\\container1\\";
+private static final String STORAGE_PATH = "/Users/lib-user/Downloads/volume/";
 
     @PostMapping("/calculatesum")
-    public ResponseEntity<Object> calculateSum(@RequestBody Map<String, String> request) {
+    public ResponseEntity<Map<String,Object>> calculateSum(@RequestBody Map<String, String> request) {
         try {
             String fileName = request.get("file");
             String product = request.get("product");
 
             String filePath = STORAGE_PATH + fileName;
+            System.out.println("FilePath --> " + filePath);
             File file = new File(filePath);
             if (!file.exists()) {
+                System.out.println("Line - 35");
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body(createResult(fileName, "File not found.", null));
             }
 
@@ -38,17 +41,34 @@ private static final String STORAGE_PATH = "E:\\DALHOUSIE COURSEWORK\\Cloud Comp
             try {
                 fileContent = readFileContent(filePath);
             } catch (IOException e) {
+                System.out.println("line 43");
                 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(createResult(fileName, "Error reading file content.", null));
             }
 
-            if (fileName == null || fileName.isEmpty()) {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(createResult(null, "Invalid JSON input.", null));
-            }
-
-            int sum;
+            int sum = 0;
             try {
-                sum = calculateSumFromCSV(fileContent, product);
+                CSVReader reader = new CSVReader(new StringReader(fileContent));
+                String[] nextLine;
+
+                String[] headers = reader.readNext();
+                System.out.println(Arrays.toString(headers));
+                while ((nextLine = reader.readNext()) != null) {
+
+                    if(nextLine.length > 2) {
+                        System.out.println("Error --> line 58");
+                        throw new Exception();
+                    }
+//
+                    String rowProduct = nextLine[0].trim();
+                    if (rowProduct.equals(product)) {
+                        System.out.println("hello inside sum");
+                        sum += Integer.parseInt(nextLine[1].trim());
+                        System.out.println("sum is: " + sum);
+                    }
+
+                }
             } catch (Exception e) {
+                System.out.println("line 51");
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(createResult(fileName, "Input file not in CSV format.", null));
             }
 
@@ -80,7 +100,7 @@ private static final String STORAGE_PATH = "E:\\DALHOUSIE COURSEWORK\\Cloud Comp
             int sum = 0;
 
             String[] headers = reader.readNext();
-
+            System.out.println(Arrays.toString(headers));
             if(headers.length > 2){
                 return -1;
             }
